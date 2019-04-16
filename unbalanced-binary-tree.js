73ad78057313a18c84cad4e5edd041b9527a46b0
Nicolas Meinen <meinen.nicolas@gmail.com>
2019-04-16 11:29:06 -0300

    Update unbalanced-binary-tree.js

--- unbalanced-binary-tree.js
+++ unbalanced-binary-tree.js
@@ -11,17 +11,16 @@ const tree = (root_node, insert) => ({
 
 const shouldGoLeft = (_new, old) => _new.self < old.self
 
+const baseRecursionOn = (self, left_test, self_node, new_node) => {
+  const direction = left_test(new_node, self_node) ? "left" : "right"
+
+  if (!self_node[direction]) return self_node[direction] = new_node
+
+  return self.insert(self_node[direction], new_node)
+}
+
 const own_tree = tree(node(1), function(self_node, new_node) {
   if (!self_node) return this.root_node = new_node
 
-  if (shouldGoLeft(new_node, self_node)) { //must recurse to the left
-    if (!self_node.left) return self_node.left = new_node //if no left, put left
-
-    return this.insert(self_node.left, new_node)
-  }
-
-  //from this point on, we must go right!
-  if (!self_node.right) return self_node.right = new_node
-
-  return this.insert(self_node.right, new_node)
+  return baseRecursionOn(this, shouldGoLeft, self_node, new_node)
 })
